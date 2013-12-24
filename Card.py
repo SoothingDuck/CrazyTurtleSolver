f8b288645497fa09c39169755943064d1e1e221d
MrCanard <yvanaillet@yahoo.fr>
2013-12-24 15:53:07 +0100

    carte en cours

--- Card.py
+++ Card.py
@@ -1,5 +1,54 @@
 
+from Turtle import Turtle
+from Color import Color
+from TurtlePart import TurtlePart
+
+class CardException(Exception):
+    pass
+
 class Card:
-    def __init__(self):
-        pass
+    def __init__(self, card_string):
+        """Initialisation d'une carte avec des tortues
+        Ordre => North, East, South, West
+        Exemple : CRCVTVTJ
+        North : Corps Rouge
+        East  : Corps Vert
+        South : Tete Verte
+        West  : Tete Jaune
+        """
+
+        if len(card_string) != 8:
+            raise CardException
+
+        self.turtle_north = Turtle(card_string[0:2])
+        self.turtle_east = Turtle(card_string[2:4])
+        self.turtle_south = Turtle(card_string[4:6])
+        self.turtle_west = Turtle(card_string[6:8])
+
+    def matches_west(self, other):
+        """Test si la tortue a l'ouest de la carte
+        matche avec la tortue a l'est de l'autre carte
+        """
+        return self.turtle_west.matches(other.turtle_east)
+
+    def matches_east(self, other):
+        """Test si la tortue a l'est de la carte
+        matche avec la tortue a l'ouest de l'autre carte
+        """
+        return self.turtle_east.matches(other.turtle_west)
+
+    def matches_north(self, other):
+        """Test si la tortue au nord de la carte
+        matche avec la tortue au sud de l'autre carte
+        """
+        return self.turtle_north.matches(other.turtle_south)
+
+
+    def matches_south(self, other):
+        """Test si la tortue au sud de la carte
+        matche avec la tortue au nord de l'autre carte
+        """
+        return self.turtle_south.matches(other.turtle_north)
+
+
 
---
+++ CardTest.py
@@ -0,0 +1,77 @@
+from Card import *
+from Turtle import *
+import unittest
+
+class CardTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.c1 = Card("CRCVTVTJ")
+        self.c2 = Card("CVCJTRTV")
+        self.c3 = Card("CBCBTBTB")
+        self.c4 = Card("CBCBTBTB")
+
+    def test_init(self):
+
+        self.assertRaises(CardException, Card, "CR")
+
+        self.assertTrue(self.c1.turtle_north, Turtle("CR"))
+        self.assertTrue(self.c1.turtle_east, Turtle("CV"))
+        self.assertTrue(self.c1.turtle_south, Turtle("TV"))
+        self.assertTrue(self.c1.turtle_west, Turtle("TJ"))
+
+        self.assertTrue(self.c2.turtle_north, Turtle("CV"))
+        self.assertTrue(self.c2.turtle_east, Turtle("CJ"))
+        self.assertTrue(self.c2.turtle_south, Turtle("TR"))
+        self.assertTrue(self.c2.turtle_west, Turtle("TV"))
+
+
+    def test_rotate_right(self):
+        
+        self.c2 = self.c1.rotate_right()
+
+        self.assertTrue(self.c2.turtle_east, Turtle("CR"))
+        self.assertTrue(self.c2.turtle_south, Turtle("CV"))
+        self.assertTrue(self.c2.turtle_west, Turtle("TV"))
+        self.assertTrue(self.c2.turtle_north, Turtle("TJ"))
+
+        self.assertTrue(self.c1.turtle_north, Turtle("CR"))
+        self.assertTrue(self.c1.turtle_east, Turtle("CV"))
+        self.assertTrue(self.c1.turtle_south, Turtle("TV"))
+        self.assertTrue(self.c1.turtle_west, Turtle("TJ"))
+
+    def test_rotate_left(self):
+
+        self.c2 = self.c1.rotate_left()
+
+        self.assertTrue(self.c2.turtle_west, Turtle("CR"))
+        self.assertTrue(self.c2.turtle_north, Turtle("CV"))
+        self.assertTrue(self.c2.turtle_east, Turtle("TV"))
+        self.assertTrue(self.c2.turtle_south, Turtle("TJ"))
+
+        self.assertTrue(self.c1.turtle_north, Turtle("CR"))
+        self.assertTrue(self.c1.turtle_east, Turtle("CV"))
+        self.assertTrue(self.c1.turtle_south, Turtle("TV"))
+        self.assertTrue(self.c1.turtle_west, Turtle("TJ"))
+
+    def test_matches(self):
+
+        self.assertTrue(self.c1.matches_west(self.c2))
+        self.assertTrue(self.c1.matches_east(self.c2))
+        self.assertTrue(self.c1.matches_north(self.c2))
+        self.assertTrue(self.c1.matches_south(self.c2))
+
+        self.assertTrue(self.c2.matches_west(self.c1))
+        self.assertTrue(self.c2.matches_east(self.c1))
+        self.assertTrue(self.c2.matches_north(self.c1))
+        self.assertTrue(self.c2.matches_south(self.c1))
+
+        self.assertFalse(self.c1.matches_west(self.c3))
+        self.assertFalse(self.c1.matches_east(self.c3))
+        self.assertFalse(self.c1.matches_north(self.c3))
+        self.assertFalse(self.c1.matches_south(self.c3))
+
+    def test_equality(self):
+        raise
+
+if __name__ == "__main__":
+    unittest.main()
