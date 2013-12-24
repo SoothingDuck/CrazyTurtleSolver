c336c5ec999893cc71d962b7c4c2de7d1cb1d130
MrCanard <yvanaillet@yahoo.fr>
2013-12-24 15:05:06 +0100

    Tortue ok

--- Turtle.py
+++ Turtle.py
@@ -2,5 +2,45 @@
 import Color
 import TurtlePart
 
-class Turtle:
+class TurtleException(Exception):
     pass
+
+class Turtle:
+    
+    def __init__(self, init_string):
+        """Initialisation
+
+        Exemples : 
+        CR == Corps Rouge
+        TV == Tete Verte
+        """
+        if len(init_string) != 2:
+            raise TurtleException
+
+        turtle_part_string = init_string[0]
+
+        self.part = TurtlePart.TurtlePart(turtle_part_string)
+
+        color_string = init_string[1]
+
+        self.color = Color.Color(color_string)
+        
+    def __eq__(self, other):
+        """Test d'egalite entre tortues"""
+        return (self.color == other.color) and (self.part == other.part)
+
+    def __str__(self):
+        """Affichage des caracteristiques de la tortue
+        Exemples :
+        Tete Bleue
+        Corps Jaune
+        """
+        if self.part == TurtlePart.TurtlePart("C"):
+            return "%s %s" % (self.part, self.color)
+        else:
+            if self.color == Color.Color("B"):
+                return "%s Bleue" % (self.part)
+            elif self.color == Color.Color("V"):
+                return "%s Verte" % (self.part)
+            else:
+                return "%s %s" % (self.part, self.color)
---
+++ TurtleTest.py
@@ -0,0 +1,62 @@
+from Turtle import *
+from Color import *
+from TurtlePart import *
+import unittest
+
+class TurtleTest(unittest.TestCase):
+    
+    def test_initturtle(self):
+        t1 = Turtle("CR")
+        
+        self.assertEqual(t1.part, TurtlePart("C"))
+        self.assertEqual(t1.color, Color("R"))
+
+        t2 = Turtle("TV")
+
+        self.assertEqual(t2.part, TurtlePart("T"))
+        self.assertEqual(t2.color, Color("V"))
+
+        t3 = Turtle("CJ")
+
+        self.assertEqual(t3.part, TurtlePart("C"))
+        self.assertEqual(t3.color, Color("J"))
+
+        t4 = Turtle("TB")
+
+        self.assertEqual(t4.part, TurtlePart("T"))
+        self.assertEqual(t4.color, Color("B"))
+
+    def test_equality(self):
+        t1 = Turtle("TB")
+        t2 = Turtle("TB")
+
+        self.assertEqual(t1, t2)
+
+    def test_inequality(self):
+        t1 = Turtle("TB")
+        t2 = Turtle("TV")
+        t3 = Turtle("CB")
+
+        self.assertNotEqual(t1, t2)
+        self.assertNotEqual(t2, t3)
+        self.assertNotEqual(t3, t1)
+
+    def test_wrongdef(self):
+
+        self.assertRaises(TurtleException, Turtle, "TBB")
+        self.assertRaises(TurtlePartException, Turtle, "BB")
+        self.assertRaises(ColorException, Turtle, "CY")
+
+    def test_string(self):
+
+        t = Turtle("TV")
+        self.assertEqual(str(t), "Tete Verte")
+
+        t = Turtle("TB")
+        self.assertEqual(str(t), "Tete Bleue")
+
+        t = Turtle("CJ")
+        self.assertEqual(str(t), "Corps Jaune")
+
+if __name__ == "__main__":
+    unittest.main()
