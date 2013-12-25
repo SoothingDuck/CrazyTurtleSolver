e2565393e20633212e920feafa9198fce306cd97
MrCanard <yvanaillet@yahoo.fr>
2013-12-25 19:05:23 +0100

    deplacement dans le setup

--- CardSetTest.py
+++ CardSetTest.py
@@ -4,20 +4,24 @@ import unittest
 
 class CardSetTest(unittest.TestCase):
 
+    def setUp(self):
+        self.a = CardSet()
+        self.a.init_crazy_turle_cardset()
+
     def test_init(self):
         
-        a = CardSet()
-        a.init_crazy_turle_cardset()
 
-        self.assertTrue(Card("TVCRTJTB") in a)
-        self.assertTrue(Card("CBCVTBTR") in a)
-        self.assertTrue(Card("TRCJCRTV") in a)
-        self.assertTrue(Card("CJCVTBCV") in a)
-        self.assertTrue(Card("CBCJTRTV") in a)
-        self.assertTrue(Card("TRTRCVTJ") in a)
-        self.assertTrue(Card("CBTJCRTJ") in a)
-        self.assertTrue(Card("CRTBTJCJ") in a)
-        self.assertTrue(Card("TVCBCVCB") in a)
+        self.assertTrue(Card("TVCRTJTB") in self.a)
+        self.assertTrue(Card("CBCVTBTR") in self.a)
+        self.assertTrue(Card("TRCJCRTV") in self.a)
+        self.assertTrue(Card("CJCVTBCV") in self.a)
+        self.assertTrue(Card("CBCJTRTV") in self.a)
+        self.assertTrue(Card("TRTRCVTJ") in self.a)
+        self.assertTrue(Card("CBTJCRTJ") in self.a)
+        self.assertTrue(Card("CRTBTJCJ") in self.a)
+        self.assertTrue(Card("TVCBCVCB") in self.a)
+
+    
 
 if __name__ == "__main__":
     unittest.main()
