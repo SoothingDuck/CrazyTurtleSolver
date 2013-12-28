f18835797058f0cf632c54350f1ecd72c244c8af
MrCanard <yvanaillet@yahoo.fr>
2013-12-28 10:59:50 +0100

    launch all test

---
+++ AllTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+from ColorTest import ColorTest
+from TurtlePartTest import TurtlePartTest
+from TurtleTest import TurtleTest
+from CardTest import CardTest
+from CardSetTest import CardSetTest
+from GridTest import GridTest
+
+if __name__ == "__main__":
+    unittest.main()
